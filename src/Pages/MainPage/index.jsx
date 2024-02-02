e7db425b771f63d618660958d5973f41a3912526
MinJun Park <me@zzunipark.com>
2024-02-02 19:38:51 +0900

    ✨:: 주제를 입력하는 팝업을 추가했어요

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -6,8 +6,11 @@ import SketchPng from "../../Assets/pngs/Sketch.png";
 import { SwipeRightArrowSVG, SwipeLeftArrowSVG } from "../../Assets/svgs";
 
 function MainPage() {
+  const [inputValue, setInputValue] = useState("");
   const [topicsExist, setTopicsExist] = useState(true);
   const [slidePx, setSlidePx] = useState(0);
+  const [typeTopicPopUpEnabled, setTypeTopicPopUpEnabled] = useState(false);
+  const [sameTopicAlreadyExist, setSameTopicAlreadyExist] = useState(false);
 
   const handleSwipeRight = () => {
     const newSlidePx = slidePx - 1200;
@@ -19,40 +22,80 @@ function MainPage() {
     setSlidePx(newSlidePx);
   };
 
+  const handleTypeTopicPopUp = () => {
+    setTypeTopicPopUpEnabled(!typeTopicPopUpEnabled);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const testFunc = () => {
+    setSameTopicAlreadyExist(!sameTopicAlreadyExist);
+  };
+
   return (
     <>
       <Navbar />
       {topicsExist ? (
-        <s.TopicsMainContainer>
-          <s.TopicsContainer
-            style={{
-              transform: `translateX(${slidePx}px)`,
-              transition: "transform 0.3s ease-in-out",
-            }}
-          >
-            <ThreadBox />
-            <ThreadBox />
-            <ThreadBox />
-            <ThreadBox />
-            <ThreadBox />
-          </s.TopicsContainer>
-          <s.CreateThreadText>
-            찾으시는 스레드가 없나요?{" "}
-            <s.CreateThreadButtonText>스레드 만들기</s.CreateThreadButtonText>
-          </s.CreateThreadText>
-          <s.SwipeRightArrowButton
-            onClick={handleSwipeRight}
-            style={{ display: slidePx <= -1500 ? "none" : "flex" }}
-          >
-            <SwipeRightArrowSVG />
-          </s.SwipeRightArrowButton>
-          <s.SwipeLeftArrowButton
-            onClick={handleSwipeLeft}
-            style={{ display: slidePx >= 0 ? "none" : "flex" }}
-          >
-            <SwipeLeftArrowSVG />
-          </s.SwipeLeftArrowButton>
-        </s.TopicsMainContainer>
+        <>
+          <s.TopicsMainContainer>
+            <s.TopicsContainer
+              style={{
+                transform: `translateX(${slidePx}px)`,
+                transition: "transform 0.3s ease-in-out",
+              }}
+            >
+              <ThreadBox />
+              <ThreadBox />
+              <ThreadBox />
+              <ThreadBox />
+              <ThreadBox />
+            </s.TopicsContainer>
+            <s.CreateThreadText>
+              찾으시는 스레드가 없나요?{" "}
+              <s.CreateThreadButtonText onClick={handleTypeTopicPopUp}>
+                스레드 만들기
+              </s.CreateThreadButtonText>
+            </s.CreateThreadText>
+            <s.SwipeRightArrowButton
+              onClick={handleSwipeRight}
+              style={{ display: slidePx <= -1500 ? "none" : "flex" }}
+            >
+              <SwipeRightArrowSVG />
+            </s.SwipeRightArrowButton>
+            <s.SwipeLeftArrowButton
+              onClick={handleSwipeLeft}
+              style={{ display: slidePx >= 0 ? "none" : "flex" }}
+            >
+              <SwipeLeftArrowSVG />
+            </s.SwipeLeftArrowButton>
+          </s.TopicsMainContainer>
+          {typeTopicPopUpEnabled && (
+            <>
+              <s.PopUpBackground onClick={handleTypeTopicPopUp} />
+              <s.TypeTopicPopUpContainer>
+                <s.TypeTopicPopUpTitle>
+                  주제를 입력해주세요
+                </s.TypeTopicPopUpTitle>
+                <s.TypeTopicPopUpInput
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  className={sameTopicAlreadyExist ? "error" : ""}
+                />
+                {sameTopicAlreadyExist && (
+                  <s.ErrorMessage>이미 존재하는 스레드입니다.</s.ErrorMessage>
+                )}
+                <s.TypeTopicPopUpButton
+                  onClick={testFunc}
+                  className={inputValue ? "active" : ""}
+                >
+                  확인
+                </s.TypeTopicPopUpButton>
+              </s.TypeTopicPopUpContainer>
+            </>
+          )}
+        </>
       ) : (
         <s.MainContainer>
           <s.ThreadNotFoundBox>
--- src/Pages/MainPage/style.js
+++ src/Pages/MainPage/style.js
@@ -23,6 +23,16 @@ export const TopicsContainer = styled.div`
   position: fixed;
 `;
 
+export const PopUpBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(64, 64, 64, 0.5);
+  z-index: 1;
+`;
+
 export const ThreadNotFoundBox = styled.div`
   display: flex;
   justify-content: center;
@@ -106,4 +116,80 @@ export const SwipeLeftArrowButton = styled.div`
   align-items: center;
   border-radius: 50%;
   cursor: pointer;
+`;
+
+export const TypeTopicPopUpContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  flex-direction: column;
+  width: 460px;
+  height: 300px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border-radius: 20px;
+  z-index: 100;
+`;
+
+export const TypeTopicPopUpTitle = styled.div`
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  letter-spacing: -0.26px;
+  margin-bottom: 32px;
+`;
+
+export const TypeTopicPopUpInput = styled.input`
+  display: flex;
+  width: 280px;
+  height: 50px;
+  padding: 8px 16px;
+  align-items: center;
+  gap: 10px;
+  border-radius: 20px;
+  border: 1px solid #d9d9d9;
+  margin-bottom: 62px;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: -0.22px;
+
+  &.error {
+    border: 1px solid #ff3131;
+  }
+`;
+
+export const ErrorMessage = styled.div`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.14px;
+  color: #ff3131;
+  position: absolute;
+  margin-top: 50px;
+  margin-right: 120px;
+`;
+
+export const TypeTopicPopUpButton = styled.div`
+  display: flex;
+  width: 120px;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 10px;
+  background-color: #d9d9d9;
+  cursor: pointer;
+  color: white;
+
+  &.active {
+    background-color: #5d9bff;
+  }
 `;
