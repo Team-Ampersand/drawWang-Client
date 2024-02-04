8d5032f4fc45706d98c257f14c377c3f6ac671bd
MinJun Park <me@zzunipark.com>
2024-02-04 22:26:21 +0900

    ✨:: 명예의전당 페이지에 스레드박스를 추가했어요

--- src/Pages/HallOfFamePage/index.jsx
+++ src/Pages/HallOfFamePage/index.jsx
@@ -1,41 +1,115 @@
 import React, { useState } from "react";
 import Navbar from "../../Components/Navbar";
 import * as s from "./style";
-import { BlueCircleSVG, GrayCircleSVG } from "../../Assets/svgs";
+import {
+  BlueCircleSVG,
+  GrayCircleSVG,
+  SwipeRightArrowSVG,
+  SwipeLeftArrowSVG,
+} from "../../Assets/svgs";
+import ThreadBox from "../../Components/ThreadBox/HallOfFame";
 
 function HallOfFamePage() {
   const [selected, setSelected] = useState("today");
+  const [hallOfFameExist, setHallOfFameExist] = useState(false);
+  const [slidePx, setSlidePx] = useState(0);
+
+  const handleSwipeRight = () => {
+    const newSlidePx = slidePx - 1200;
+    setSlidePx(newSlidePx);
+  };
+
+  const handleSwipeLeft = () => {
+    const newSlidePx = slidePx + 1200;
+    setSlidePx(newSlidePx);
+  };
 
   return (
     <>
-      <Navbar />
-      <s.MainContainer>
-        <s.HallOfFameBox>
-          <s.HallOfFameImg />
-          <s.HallOfFameTextBox>
-            <s.HallOfFameTitle>
-              명예의 전당에 오른 그림이 없어요
-            </s.HallOfFameTitle>
-            <s.HallOfFameDesc>
-              가장 많은 좋아요를 받아 명예의 전당에 올라보세요!
-            </s.HallOfFameDesc>
-          </s.HallOfFameTextBox>
-        </s.HallOfFameBox>
-        <s.SelectDateBox>
-          <s.SelectWrapper onClick={() => setSelected("today")}>
-            {selected === "today" ? <BlueCircleSVG /> : <GrayCircleSVG />}
-            <s.SelectDateText className={selected === "today" ? "active" : ""}>
-              오늘
-            </s.SelectDateText>
-          </s.SelectWrapper>
-          <s.SelectWrapper onClick={() => setSelected("all")}>
-            {selected === "all" ? <BlueCircleSVG /> : <GrayCircleSVG />}
-            <s.SelectDateText className={selected === "all" ? "active" : ""}>
-              전체
-            </s.SelectDateText>
-          </s.SelectWrapper>
-        </s.SelectDateBox>
-      </s.MainContainer>
+      {hallOfFameExist ? (
+        <>
+          <Navbar />
+          <s.HallOfFameMainContainer>
+            <s.HallOfFameThreadBoxTitle>명예의 전당</s.HallOfFameThreadBoxTitle>
+            <s.HallOfFameContainer
+              style={{
+                transform: `translateX(${slidePx}px)`,
+                transition: "transform 0.3s ease-in-out",
+              }}
+            >
+              <ThreadBox />
+              <ThreadBox />
+              <ThreadBox />
+              <ThreadBox />
+            </s.HallOfFameContainer>
+            <s.SelectDateBox>
+              <s.SelectWrapper onClick={() => setSelected("today")}>
+                {selected === "today" ? <BlueCircleSVG /> : <GrayCircleSVG />}
+                <s.SelectDateText
+                  className={selected === "today" ? "active" : ""}
+                >
+                  오늘
+                </s.SelectDateText>
+              </s.SelectWrapper>
+              <s.SelectWrapper onClick={() => setSelected("all")}>
+                {selected === "all" ? <BlueCircleSVG /> : <GrayCircleSVG />}
+                <s.SelectDateText
+                  className={selected === "all" ? "active" : ""}
+                >
+                  전체
+                </s.SelectDateText>
+              </s.SelectWrapper>
+            </s.SelectDateBox>
+            <s.SwipeRightArrowButton
+              onClick={handleSwipeRight}
+              style={{ display: slidePx <= -1500 ? "none" : "flex" }} // 추후 서버에서 받아온 데이터로 변경
+            >
+              <SwipeRightArrowSVG />
+            </s.SwipeRightArrowButton>
+            <s.SwipeLeftArrowButton
+              onClick={handleSwipeLeft}
+              style={{ display: slidePx >= 0 ? "none" : "flex" }}
+            >
+              <SwipeLeftArrowSVG />
+            </s.SwipeLeftArrowButton>
+          </s.HallOfFameMainContainer>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <s.MainContainer>
+            <s.HallOfFameBox>
+              <s.HallOfFameImg />
+              <s.HallOfFameTextBox>
+                <s.HallOfFameTitle>
+                  명예의 전당에 오른 그림이 없어요
+                </s.HallOfFameTitle>
+                <s.HallOfFameDesc>
+                  가장 많은 좋아요를 받아 명예의 전당에 올라보세요!
+                </s.HallOfFameDesc>
+              </s.HallOfFameTextBox>
+            </s.HallOfFameBox>
+            <s.SelectDateBox>
+              <s.SelectWrapper onClick={() => setSelected("today")}>
+                {selected === "today" ? <BlueCircleSVG /> : <GrayCircleSVG />}
+                <s.SelectDateText
+                  className={selected === "today" ? "active" : ""}
+                >
+                  오늘
+                </s.SelectDateText>
+              </s.SelectWrapper>
+              <s.SelectWrapper onClick={() => setSelected("all")}>
+                {selected === "all" ? <BlueCircleSVG /> : <GrayCircleSVG />}
+                <s.SelectDateText
+                  className={selected === "all" ? "active" : ""}
+                >
+                  전체
+                </s.SelectDateText>
+              </s.SelectWrapper>
+            </s.SelectDateBox>
+          </s.MainContainer>
+        </>
+      )}
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <MainPage />
+    <HallOfFamePage />
   </React.StrictMode>
 );
